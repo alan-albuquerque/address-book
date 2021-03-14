2df0eac3090bb697c62611621b449d756a5b8593
Alan Nascimento <alanteclog@gmail.com>
2021-03-14 13:04:03 -0300

    create custom button component

---
+++ src/components/core/Button.tsx
@@ -0,0 +1,39 @@
+import React, { ButtonHTMLAttributes, FunctionComponent } from 'react';
+import classNames from 'classnames';
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  icon?: React.ReactElement;
+  className?: string;
+}
+
+const Button: FunctionComponent<ButtonProps> = props => {
+  const { type, icon, children, className, ...rest } = props;
+
+  const buttonClassNames = classNames(
+    'flex',
+    'justify-center',
+    'items-center',
+    'align-center',
+    'text-gray-500',
+    'hover:text-purple-800',
+    'p-1.5',
+    'rounded-3xl',
+    'hover:bg-gray-200',
+    'active:bg-gray-300',
+    'focus:outline-none',
+    className,
+  );
+
+  /* eslint-disable react/button-has-type */
+  // 'react/button-has-type' rule requires the 'type' property to be a static string,
+  // but this does not work if we are creating an abstract button component
+  return (
+    <button type={type || 'button'} className={buttonClassNames} {...rest}>
+      {icon}
+      {children && <div className="ml-2">{children}</div>}
+    </button>
+  );
+  /* eslint-enable react/button-has-type */
+};
+
+export default Button;
